6cdbcf064c701b6f39580bf16b50757de0d5de4c
ArthurRobein <arthur.robein@gmail.com>
2023-04-22 16:37:21 +0200

    Added clock and reorganize start

--- start.scm
+++ start.scm
@@ -36,30 +36,17 @@
     )
   )
 
+  (define hero_hp_bar
+    (lambda (wid)
+      (ywCanvasNewRectangle wid 0 300 100 100 "rgba: 0 0 0 255")
+    )
+  )
+
   (define tmst_action
     (lambda (wid events)
-      (let ((str "Hello Action\n" )
-          (x (get_sprite_pos wid "first" "x"))
-          (y (get_sprite_pos wid "first" "y"))
-          (w (get_sprite_pos wid "first" "w"))
-          (h (get_sprite_pos wid "first" "h"))
-          (enemy_max_hp (get_stat wid "first" "hp"))
-          (enemy_atk (get_stat wid "first" "atk"))
-          (enemy_def (get_stat wid "first" "def"))
-          (enemy_crit (get_stat wid "first" "%crit"))
-          (hp 10)
-          (atk 4)
-          (def 2)
-          (crit 10)
-        )
+      (let ((str "Hello Action\n" ))
         (begin
           (display str)
-          (yePushBack wid (ywCanvasNewImg wid 550 (- 300 h)
-                      (yeGetString(yeGet(yeGet(yeGet wid "json") "first") "enemy-img"))
-                      (ywRectCreate x y w h)) "monster")
-          (yePushBack wid (ywCanvasNewImg wid 350 400
-                      "spritesheets/Clock.png"
-                      (ywRectCreate 0 0 100 100)) "clock")
           ;;
           ;;(ywCanvasMoveObjXY (yeGet wid "car") 1 0)
         )
@@ -69,21 +56,40 @@
 
   (define tmst_init
     (lambda (wid unues_type)
-      (begin
+      (let
+        ((unused (yePushBack wid (ygFileToEnt YJSON "rooms.json") "json"))
+        (x (get_sprite_pos wid "first" "x"))
+        (y (get_sprite_pos wid "first" "y"))
+        (w (get_sprite_pos wid "first" "w"))
+        (h (get_sprite_pos wid "first" "h"))
+        (enemy_max_hp (get_stat wid "first" "hp"))
+        (enemy_atk (get_stat wid "first" "atk"))
+        (enemy_def (get_stat wid "first" "def"))
+        (enemy_crit (get_stat wid "first" "%crit"))
+        (hp 10)
+        (atk 4)
+        (def 2)
+        (crit 10))
+        (begin
         (display "Hello world\n")
         (ywSetTurnLengthOverwrite 100000)
         (yeCreateFunction "tmst_action" wid "action")
         ;;(ywCanvasNewTextByStr wid 10 25 "test")
         (yePushBack wid (ywCanvasNewImg wid 0 0 "cave.jpg" (ywRectCreate 0 0 1000 1000)) "cave")
         (yePushBack wid (ywCanvasNewImg wid 200 230 "spritesheets/HeroesHero.png" (ywRectCreate 9 88 36 70)) "hero")
-        (yePushBack wid (ygFileToEnt YJSON "rooms.json") "json")
+        (yePushBack wid (ywCanvasNewImg wid 550 (- 300 h)
+                      (yeGetString(yeGet(yeGet(yeGet wid "json") "first") "enemy-img"))
+                      (ywRectCreate x y w h)) "monster")
+        (yePushBack wid (ywCanvasNewImg wid 350 400
+                      "spritesheets/Clock.png"
+                      (ywRectCreate 0 0 100 100)) "clock")
         (ywCanvasNewHSegment wid 0 300 1000 "rgba: 0 0 0 255")
-        (ywCanvasNewRectangle wid 0 300 100 100 "rgba: 0 0 0 255")
         ;;(yePushBack wid (ywCanvasNewImg wid 100 100 "car.png" (ywRectCreate 100 100 100 100)) "car")
         ;; canvas widget, and set a white background
         ;; yaeString is like yeCreateString, but yeCreateString return the string,
         ;; and yae, it's parent
         (ywidNewWidget (yaeString "rgba: 255 255 255 255" wid "background") "canvas")
+        )
       )
     )
   )
