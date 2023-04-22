12fa3ab770225e1297bf62b9f335c275b7880994
ArthurRobein <arthur.robein@gmail.com>
2023-04-22 16:16:18 +0200

    Added Rectangle

--- start.scm
+++ start.scm
@@ -30,6 +30,12 @@
     )
   )
 
+  (define get_stat
+    (lambda (wid room stat)
+      (yeGetInt(yeGet(yeGet(yeGet(yeGet wid "json") room) "stats") stat))
+    )
+  )
+
   (define tmst_action
     (lambda (wid events)
       (let ((str "Hello Action\n" )
@@ -37,12 +43,24 @@
           (y (get_sprite_pos wid "first" "y"))
           (w (get_sprite_pos wid "first" "w"))
           (h (get_sprite_pos wid "first" "h"))
+          (enemy_max_hp (get_stat wid "first" "hp"))
+          (enemy_atk (get_stat wid "first" "atk"))
+          (enemy_def (get_stat wid "first" "def"))
+          (enemy_crit (get_stat wid "first" "%crit"))
+          (hp 10)
+          (atk 4)
+          (def 2)
+          (crit 10)
         )
         (begin
           (display str)
           (yePushBack wid (ywCanvasNewImg wid 550 (- 300 h)
                       (yeGetString(yeGet(yeGet(yeGet wid "json") "first") "enemy-img"))
                       (ywRectCreate x y w h)) "monster")
+          (yePushBack wid (ywCanvasNewImg wid 350 400
+                      "spritesheets/Clock.png"
+                      (ywRectCreate 0 0 100 100)) "clock")
+          ;;
           ;;(ywCanvasMoveObjXY (yeGet wid "car") 1 0)
         )
       )
@@ -58,14 +76,13 @@
         ;;(ywCanvasNewTextByStr wid 10 25 "test")
         (yePushBack wid (ywCanvasNewImg wid 0 0 "cave.jpg" (ywRectCreate 0 0 1000 1000)) "cave")
         (yePushBack wid (ywCanvasNewImg wid 200 230 "spritesheets/HeroesHero.png" (ywRectCreate 9 88 36 70)) "hero")
-        ;;(yePushBack wid (ywCanvasNewImg wid 550 124 "spritesheets/GreenMonster.png" (ywRectCreate 194 198 171 176)) "monster")
         (yePushBack wid (ygFileToEnt YJSON "rooms.json") "json")
         (ywCanvasNewHSegment wid 0 300 1000 "rgba: 0 0 0 255")
+        (ywCanvasNewRectangle wid 0 300 100 100 "rgba: 0 0 0 255")
         ;;(yePushBack wid (ywCanvasNewImg wid 100 100 "car.png" (ywRectCreate 100 100 100 100)) "car")
         ;; canvas widget, and set a white background
         ;; yaeString is like yeCreateString, but yeCreateString return the string,
         ;; and yae, it's parent
-        ;;(yePrint yeGet(wid "json"))
         (ywidNewWidget (yaeString "rgba: 255 255 255 255" wid "background") "canvas")
       )
     )
